93d9f1402ef85a3c56185c7bee439011bcb9c662
sharachchandra <sharachchandra.bhat@gmail.com>
2022-05-05 13:46:06 -0500

    cleaned up env2

--- gym_cruise_ctrl/gym_cruise_ctrl/envs/cruise_ctrl_env2.py
+++ gym_cruise_ctrl/gym_cruise_ctrl/envs/cruise_ctrl_env2.py
@@ -8,7 +8,7 @@ import numpy as np
 from os import path
 import pygame
 from pygame import gfxdraw
-from gym_cruise_ctrl.envs.input_generator import PiecewiseLinearProfile, Spline
+from gym_cruise_ctrl.envs.input_generator import InputAccGenerator
 from gym_cruise_ctrl.envs.noise_model import NoisyDepth, NoisyVel
 
 class CruiseCtrlEnv2(gym.Env):
@@ -22,7 +22,7 @@ class CruiseCtrlEnv2(gym.Env):
 		"""
 		self.max_acc = 1.5	# 1.5 m/s^2 
 
-		self.action_low = -1.0 
+		self.action_low  = -1.0
 		self.action_high = 1.0
 		self.action_space = gym.spaces.Box(low=-1.0, high=1.0, shape=(1,))
 		
@@ -36,47 +36,63 @@ class CruiseCtrlEnv2(gym.Env):
 		"""
 		self.observation_space = gym.spaces.Box(low=-np.inf, high=np.inf, shape=(3,))
 
-
 		"""
 		### Episodic Task
 		"""
-		self.delt = 0.1 # 0.1s time step
-		self.max_episode_steps = int(100/self.delt)
-		self.episode_steps = 0
-		self.done = False
+		self.delt = 0.1 									# 0.1s time step
+		self.max_episode_steps = int(100/self.delt)			# 1000 episode length
 
 		"""
 		### Environment Specifications   
 		"""
-		self.safety_dist = 5	# Required distance between the ego and front vehicle
-		self.violating_safety_dist_reward = -10*self.delt	# Reward for getting too close to the front car
-		self.fv_min_vel = 10 # 10m/s or 30mph
-		self.fv_max_vel = 30 # 30m/s or 70mph
-		self.fv_max_acc = 0.5  # 0.5m/s^2
-		self.ego_max_vel = 40 # 40m/s or 90mph 
-		self.ego_max_dist = self.ego_max_vel*self.delt # Max distance travelled in one time step
-		# self.fv_input_gen = PiecewiseLinearProfile(self.max_episode_steps, 1, 5) # class to generate the behaviour of the front vehicle
-		# self.fv_input = self.fv_input_gen.generate() # the behaviour of the front vehicle
-		self.fv_vel_list, self.fv_acc_list = Spline(self.max_episode_steps, 4, 8)
-		self.fv_vel_list = self.fv_min_vel + self.fv_vel_list*(self.fv_max_vel - self.fv_min_vel)
-		self.fv_acc_list = self.fv_acc_list*(self.fv_max_vel - self.fv_min_vel)/self.delt
-		
-		self.depth_noise_model = NoisyDepth() # depth noise model class
-		self.vel_noise_model = NoisyVel() # velocity noise model class 
-		self.noise_required = noise_required # whether noise is required or not
-		self.jerk_cost = 10
+		### Safety specifications
+		self.safety_dist 				   = 5				# Required distance between the ego and front vehicle
+		self.violating_safety_dist_reward  = -10*self.delt	# Reward for getting too close to the front car
+		self.jerk_cost 					   = 10
 		self.nominal_max_gap_keeping_error = 15
 
-		### for seed purposes 
-		self.train = train # are we training or validating? For validating, we set the seed to get constant initializations
+		### Front vehicle specifications
+		self.fv_min_vel = 10 								# 10m/s or 30mph
+		self.fv_max_vel = 30 								# 30m/s or 70mph
+		self.fv_max_acc = 0.5  								# 0.5m/s^2
+
+		### Ego vehicle specifications		
+		self.ego_max_vel  = 40 								# 40m/s or 90mph 
+		self.ego_max_dist = self.ego_max_vel*self.delt      # Max distance travelled in one time step
+		
+		### Noise specifications
+		self.noise_required    = noise_required 			# Whether noise is required or not
+		self.depth_noise_model = NoisyDepth() 				# Depth noise model class
+		self.vel_noise_model   = NoisyVel() 				# Velocity noise model class 
+
+		### For random seed purposes 
+		self.train = train 									# Are we training or validating? For validating, we set the seed to get constant initializations
+
+		"""
+		### Visualizer Parameters
+		"""
+		self.screen_dim = 500
+		self.screen = None
+
+		"""
+		### Initialziation
+		"""
+		self.InitializeEnvironmentVariables()
+
+	def InitializeEnvironmentVariables(self):
+		self.episode_steps = 0
+		self.done = False
 
 		"""
 		### Initial conditions
 		"""
 		### Front vehicle
 		self.fv_init_pos = self.InitializeFvPos()
-		self.fv_init_vel = self.fv_vel_list[0]
+		self.fv_init_vel = self.InitializeFvVel()
 		self.fv_state    = np.array([self.fv_init_pos, self.fv_init_vel], dtype=np.float32)
+		self.fv_acc_list = InputAccGenerator(self.max_episode_steps, self.delt, self.fv_init_vel, 
+											 self.fv_min_vel, self.fv_max_acc, self.fv_max_acc, 
+											 mode = 'SplineProfile')
 
 		### Ego vehicle
 		self.ego_init_pos = self.InitializeEgoPos()
@@ -89,13 +105,6 @@ class CruiseCtrlEnv2(gym.Env):
 		rel_pose = self.fv_state - self.ego_state # The state is the relative position and speed
 		self.state = np.append(rel_pose, self.ego_init_acc)
 
-		"""
-		### Visualizer Parameters
-		"""
-		self.screen_dim = 500
-		self.screen = None
-
-
 	def InitializeFvPos(self):
 		if self.train:
 			return max(20*np.random.randn() + 100, 10) # (100 +- 20)m
@@ -106,16 +115,18 @@ class CruiseCtrlEnv2(gym.Env):
 		if self.train:
 			return 0
 		else:
-			return min(self.fv_max_vel, max(5*np.random.randn() + 20, self.fv_min_vel))	# (25 +-5)m/s or 60mph
+			return min(self.fv_max_vel, max(self.fv_min_vel + np.random.rand()*(self.fv_max_vel - self.fv_min_vel), 
+											self.fv_min_vel))	# (20 +-5)m/s or 60mph
 
 	def InitializeEgoPos(self):
 		return 0
 
 	def InitializeEgoVel(self):
 		if self.train:
-			return max(5*np.random.randn() + 10, 0)	# (10 +-5)m/s or 30mph
+			return max(5*np.random.randn() + 10, 0)	# (10 +-5)m/s
 		else:
-			return max(5*np.random.randn() + 10, 0)	# (10 +-5)m/s or 30mph
+			return min(self.fv_max_vel, max(self.fv_min_vel + np.random.rand()*(self.fv_max_vel - self.fv_min_vel), 
+											self.fv_min_vel))
 
 
 	def step(self, action):
@@ -128,32 +139,30 @@ class CruiseCtrlEnv2(gym.Env):
 		### Front vehicle state transition
 		"""
 		### Acceleration input to the front vehicle
-		# fv_acc = self.fv_input[self.episode_steps]
-		# fv_acc = np.clip(fv_acc, self.action_low, self.action_high)
-		# if self.train:
-		# 	fv_acc = 0.0
-		# else:
-		# 	fv_acc = fv_acc*self.fv_max_acc*0
+		fv_acc = self.fv_acc_list[self.episode_steps]
+		if self.train:
+			fv_acc = 0.0
+		else:
+			fv_acc = fv_acc*self.fv_max_acc
 		
-		# ### Clipping acceleration to keep within velocity limits
-		# if fv_vel >= self.fv_max_vel:
-		# 	if fv_vel + fv_acc*self.delt >= self.fv_max_vel:
-		# 		fv_acc = 0.0
-		# else:
-		# 	if fv_vel + fv_acc*self.delt >= self.fv_max_vel:
-		# 		fv_acc = (self.fv_max_vel - fv_vel)/self.delt
+		### Clipping acceleration to keep within velocity limits
+		if fv_vel >= self.fv_max_vel:
+			if fv_vel + fv_acc*self.delt >= self.fv_max_vel:
+				fv_acc = 0.0
+		else:
+			if fv_vel + fv_acc*self.delt >= self.fv_max_vel:
+				fv_acc = (self.fv_max_vel - fv_vel)/self.delt
 		
-		# if fv_vel <= self.fv_min_vel:
-		# 	if fv_vel + fv_acc*self.delt <= self.fv_min_vel:
-		# 		fv_acc = 0.0
-		# else:
-		# 	if fv_vel + fv_acc*self.delt <= self.fv_min_vel:
-		# 		fv_acc = (self.fv_min_vel - fv_vel)/self.delt		
+		if fv_vel <= self.fv_min_vel:
+			if fv_vel + fv_acc*self.delt <= self.fv_min_vel:
+				fv_acc = 0.0
+		else:
+			if fv_vel + fv_acc*self.delt <= self.fv_min_vel:
+				fv_acc = (self.fv_min_vel - fv_vel)/self.delt		
 
 		### State update
-		fv_acc = self.fv_acc_list[self.episode_steps]
-		fv_vel = self.fv_vel_list[self.episode_steps]
 		fv_pos = fv_pos + fv_vel*self.delt + 0.5*fv_acc*self.delt**2
+		fv_vel = fv_vel + fv_acc*self.delt
 		self.fv_state = np.array([fv_pos, fv_vel], dtype=np.float32)
 		
 		"""
@@ -232,44 +241,7 @@ class CruiseCtrlEnv2(gym.Env):
 		return obs, reward, self.done, info
 
 	def reset(self, seed=0):
-		"""
-		### setting the fixed seed for validation purposes
-		"""
-		# if not self.train:
-		# 	np.random.seed(seed)
-		
-		"""
-		### Reset the enviornment
-		"""
-		### Resets the env and returns the starting state and done=False
-		### Reset episodic task flags and iterators to initial values
-		self.done = False
-		self.episode_steps = 0
-		
-		"""
-		### Reset states to initial conditions
-		"""
-
-		### Front vehicle
-		self.fv_vel_list, self.fv_acc_list = Spline(self.max_episode_steps, 4, 8)
-		self.fv_vel_list = self.fv_min_vel + self.fv_vel_list*(self.fv_max_vel - self.fv_min_vel)
-		self.fv_acc_list = self.fv_acc_list*(self.fv_max_vel - self.fv_min_vel)/self.delt
-
-		self.fv_init_pos = self.InitializeFvPos()
-		self.fv_init_vel = self.fv_vel_list[0] 
-		self.fv_state    = np.array([self.fv_init_pos, self.fv_init_vel], dtype=np.float32)
-		# self.fv_input = self.fv_input_gen.generate()
-
-		### Ego vehicle
-		self.ego_init_pos = self.InitializeEgoPos()
-		self.ego_init_vel = self.InitializeEgoVel()
-		self.ego_state    = np.array([self.ego_init_pos, self.ego_init_vel], dtype=np.float32)
-
-		self.ego_init_acc = 0.0
-		self.ego_jerk     = 0.0
-		
-		rel_pose = self.fv_state - self.ego_state # The state is the relative position and speed
-		self.state = np.append(rel_pose, self.ego_init_acc)
+		self.InitializeEnvironmentVariables()
 
 		### Observation 
 		obs = self.state.copy()
--- gym_cruise_ctrl/gym_cruise_ctrl/envs/input_generator.py
+++ gym_cruise_ctrl/gym_cruise_ctrl/envs/input_generator.py
@@ -5,37 +5,35 @@ from scipy.interpolate import UnivariateSpline
 
 import random as pyrandom
 
-class PiecewiseLinearProfile():
-    def __init__(self, N, n_min, n_max):
-        self.N     = N
-        self.n_max = n_max
-        self.n_min = n_min
+from torch import dsmm
 
-    def generate(self):
-        n = np.random.randint(self.n_min, self.n_max + 1)
-        
-        #anchor_x = np.array(pyrandom.sample(range(self.N), n))
-        anchor_x = np.random.choice(self.N, n, replace=False)
-        anchor_x.sort()
-        anchor_x = np.insert(anchor_x, 0, 0)
-        anchor_x = np.append(anchor_x, self.N)
-        
-        anchor_y = [np.random.uniform()]
-        for i in range(len(anchor_x)-1):
-            s = 3*(anchor_x[i+1] - anchor_x[i])/self.N
-            y_min = max(anchor_y[-1] + -1*s, -1)
-            y_max = min(anchor_y[-1] +  1*s,  1)
-            anchor_y.append(np.random.uniform(y_min, y_max))
-        
-        anchor_y = np.array(anchor_y)
-        
+def PiecewiseLinearProfile(N, n_min, n_max):
+    n = np.random.randint(n_min, n_max + 1)
+    
+    #anchor_x = np.array(pyrandom.sample(range(self.N), n))
+    anchor_x = np.random.choice(N, n, replace=False)
+    anchor_x.sort()
+    anchor_x = np.insert(anchor_x, 0, 0)
+    anchor_x = np.append(anchor_x, N)
 
-        X = np.arange(0, self.N + 1)
-        Y = np.interp(X, anchor_x, anchor_y)
+    u_min = -1
+    u_max = 1
+    
+    anchor_y = [u_min + (u_max - u_min)*np.random.uniform()]
+    for i in range(len(anchor_x)-1):
+        s = (anchor_x[i+1] - anchor_x[i])/N
+        y_min = max(anchor_y[-1] + u_min*s, u_min)
+        y_max = min(anchor_y[-1] + u_max*s, u_max)
+        anchor_y.append(np.random.uniform(y_min, y_max))
+    
+    anchor_y = np.array(anchor_y)
+    
+    X = np.arange(0, N + 1)
+    Y = np.interp(X, anchor_x, anchor_y)
 
-        return(Y)
+    return(Y)
 
-def Spline(N, n_min, n_max):
+def SplineProfile(init, N, n_min, n_max):
     k = 3
     n_min = min(k+1, n_min)
     n_max = min(k+1, n_max)
@@ -46,7 +44,7 @@ def Spline(N, n_min, n_max):
     anchor_x = np.insert(anchor_x, 0, 0)
     anchor_x = np.append(anchor_x, N)
     
-    anchor_y = [np.random.uniform()]
+    anchor_y = [init]
     for i in range(len(anchor_x)-1):
         s = (anchor_x[i+1] - anchor_x[i])/N
         y_min = max(anchor_y[-1] + -1*s, 0)
@@ -56,25 +54,34 @@ def Spline(N, n_min, n_max):
     
     x = np.arange(0, N + 1)
     spl = UnivariateSpline(anchor_x, anchor_y, k=k)
-    y = spl(x)
     spld = spl.derivative()
     yd = spld(x)
 
-    return y, yd
+    return yd
 
-# profile_gen = PiecewiseLinearProfile(100, 1, 5)
+def InputAccGenerator(N, Ts, init_vel, min_vel, max_vel, max_acc, mode = 'SplineProfile'):
+    if mode == 'SplineProfile':
+        n_min = 4
+        n_max = 8
+        init = (init_vel - min_vel)/(max_vel - min_vel)
+        fv_acc_list = SplineProfile(init, N, n_min, n_max)
+        fv_acc_list = fv_acc_list*(max_vel - min_vel)/Ts
+
+    elif mode == 'PiecewiseLinearProfile':
+        n_min = 1
+        n_max = 5
+        fv_acc_list = PiecewiseLinearProfile(N, n_min, n_max)*max_acc
+
+    else:
+        raise ValueError('InputAccGenerator Mode incorrect')
+    
+    return fv_acc_list
+
+
+# yd = InputAccGenerator(1000, 0.1, 20, 10, 30, 1, mode = 'SplineProfile')
 
 # fig, axes = plt.subplots(1,1)
 # axes.set_ylim((-1,1))
-# plt.plot(profile_gen.generate())
-
-# plt.show()
-
-# y, yd = Spline(100, 4, 8)
-
-# fig, axes = plt.subplots(1,1)
-# axes.set_ylim((-1,1))
-# plt.plot(y)
 # plt.plot(yd)
 # plt.savefig('img.png')
 # plt.show()
